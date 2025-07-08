baf6d9ef573ce50e3e853abc71dbe36d9d6dcdbf
kha1dx <162775528+kha1dx@users.noreply.github.com>
2025-07-08 21:03:07 +0300

    paperbook

--- src/PaperBook.java
+++ src/PaperBook.java
@@ -1,2 +1,33 @@
-public class PaperBook {
+public class PaperBook extends Book {
+    protected int quantity;
+    public PaperBook(String isbn, String title, String author, int yearPublished, double price, int quantity) {
+        super(isbn, title, author, yearPublished, price);
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String getBookType() {
+        return "Paper Book";
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+    public void removeFromStock(int amount) {
+        if (amount <= quantity) {
+            quantity -= amount;
+            System.out.println(quantity+ " Remaining items in stock" );
+        } else {
+            System.out.println("Not enough stock to remove " + amount + " items.");
+        }
+    }
+    public void addToStock(int amount) {
+        if (amount > 0) {
+            quantity += amount;
+            System.out.println(quantity + " Remaining items in stock");
+        } else {
+            System.out.println("Cannot add a non-positive amount to stock.");
+        }
+    }
+
 }
